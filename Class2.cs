3c11b4f5bc5c0bb6ccebdbcde7b865c26381e149
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-08 23:23:05 +0530

    prob 2

---
+++ Class2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    public class employee
+    {
+        private int _empid;
+
+        public int EmpId
+        {
+            get { return _empid; }
+            set { _empid = value; }
+        }
+        private string _empname;
+
+        public string Empname
+        {
+            get { return _empname; }
+            set { _empname = value; }
+        }
+
+        private int _deptno;
+
+        public int Deptno
+        {
+            get { return _deptno; }
+            set { _deptno = value; }
+        }
+
+        private float _sal;
+
+        public float Sal
+        {
+            get { return _sal; }
+            set { _sal = value; }
+        }
+
+        private string _mgr;
+
+        public string Mgr
+        {
+            get { return _mgr; }
+            set { _mgr = value; }
+        }
+
+
+    }
+}
