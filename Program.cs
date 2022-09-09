c6f95dce848f99f5e4169693b7b4707cedde4bd3
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-09 09:37:38 +0530

    prob 5

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menu_driven
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee()
+            {
+                EmpId = 101,
+                EmpName = "Ram",
+                DeptNo = 12,
+                Salary = 32456,
+                Manager = "Deepak"
+            };
+
+            Employee emp2 = new Employee()
+            {
+                EmpId = 102,
+                EmpName = "Avi",
+                DeptNo = 14,
+                Salary = 29276,
+                Manager = "Aman"
+            };
+            List<Employee> lstEmployess = new List<Employee>();
+            lstEmployess.Add(emp1);
+            lstEmployess.Add(emp2);
+            EligibleToPromotion eligibleTopromote = new EligibleToPromotion(Program.Promote);
+            Employee.PromoteEmployee(lstEmployess, eligibleTopromote);
+            Console.ReadKey();
+        }
+        public static bool Promote(Employee employee)
+        {
+            if (employee.Salary > 30000)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
